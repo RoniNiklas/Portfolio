f65f6e73710f6f1160c8f16ac2a8b5cb6120df34
RoniNiklas <bibburi@hotmail.com>
2020-08-22 20:19:56 +0300

    updated in august

--- src/components/JS.js
+++ src/components/JS.js
@@ -8,10 +8,10 @@ class JS extends Component {
           <div style={{ maxWidth: "800px", width: "100%", marginLeft: "auto", marginRight: "auto", overFlowWrap: "break-word" }}>
             <h1> ChatApp - project </h1>
             <p>
-              Description: A very much work-in-progress app for chatting via browser. Uses socket.io for realtime message transfer, and mongodb for saving conversations. UI is currently quite ugly.
+              Description: A very much work-in-progress app for video-conferencing. It allows video conferencing for multiple participants, but the code is a bit sketchy and it only functions like 2/3rds of the time. It is currently more reliable on Firefox than other browsers. It allows for reliable real-time chatting in the conference rooms however.
           </p>
             <p>
-              Technologies: React, Node, Express, Mongoose, React-router-dom, React-bootstrap and Socket.io.
+              Technologies: React, Node, Express, Mongoose, React-router-dom, React-bootstrap, MediaDevices API, WebRTC, and Socket.io.
           </p>
             <p style={{ overflowWrap: "break-word" }}>
               Available: <a href="https://roninchatapp.herokuapp.com/"> https://roninchatapp.herokuapp.com/</a>
@@ -27,14 +27,20 @@ class JS extends Component {
             <p>
               Description: A collection of smaller React projects. Includes:
               <p>
-                - an app that displays a list of restaurants from a JSON and allows for sorting and selecting one randomly.
+                - Application App for Wolt - A restaurant lister and list sorter done as a part of the application process for Wolt's summer internship.
               </p>
               <p>
-                - an app that finds you a public transport route from where you are to the Eficode offices in Helsinki and displays it on a map.
-               </p>
+                - Application App for Eficode -  A public transport route finding app that shows you between one and three ways of getting to the Eficode offices in Helsinki. Done as a part of the application process for Eficode's summer internship.
+             </p>
+              <p>
+                - Fitness App - A compilation of helpful tools to help you in physical training. Currently includes a timer and a weight calculator. **WIP**
+              </p>
+              <p>
+                - Dyslexia App - An app that is basically a reader, where you can alter the text's font, spacing, colors etc. Also uses the Web Speech API and the Speech Synthesis API to enable speech-to-text and text-to-speech to make text-handling easier.
+              </p>
             </p>
             <p>
-              Technologies: React, Node, Express.
+              Technologies: React, Node, Express, Web Speech API, and Speech Synthesis API.
             </p>
             <p style={{ overflowWrap: "break-word" }}>
               Available: <a href="https://roninreactprojs.herokuapp.com/"> https://roninreactprojs.herokuapp.com/</a>
